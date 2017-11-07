b3a68233ba3dcfa765c66ddeb8b66efa365970d7
Nackmann <noahnba2003@gmail.com>
2017-11-06 18:19:53 -0700

    Commit 2 Shortened by Increment Decrement

--- Move.cs
+++ Move.cs
@@ -35,19 +35,20 @@ public class Move : MonoBehaviour {
 
         if (moveRight)
         {
-            newPosition = newPosition + new Vector3(speed * Time.deltaTime, 0, 0);
+            //newPosition = newPosition + new Vector3(speed * Time.deltaTime, 0, 0);
+            newPosition += new Vector3(speed * Time.deltaTime, 0, 0);
         }
         if (moveLeft)
         {
-            newPosition = newPosition - new Vector3(speed * Time.deltaTime, 0, 0);
+            newPosition -= new Vector3(speed * Time.deltaTime, 0, 0);
         }
         if (moveUp)
         {
-            newPosition = newPosition - new Vector3(0, speed * Time.deltaTime, 0);
+            newPosition += new Vector3(0, speed * Time.deltaTime, 0);
         }
         if (moveDown)
         {
-            newPosition = newPosition - new Vector3(0, speed * Time.deltaTime, 0);
+            newPosition -= new Vector3(0, speed * Time.deltaTime, 0);
         }
         transform.position = newPosition;
 	}
