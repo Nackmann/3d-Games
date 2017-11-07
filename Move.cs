6c9ddd6f820a977d0c1f45c6988aa4c03e1749af
Nackmann <noahnba2003@gmail.com>
2017-11-06 19:18:12 -0700

    Single Control Added

--- Move.cs
+++ Move.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Move : MonoBehaviour { 
     public float speed = 5;
     private Rigidbody rb;
- 
 
+    public bool control = false;
 
 	// Use this for initialization
 	void Start () {//called when object is created
@@ -15,10 +15,11 @@ public class Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {//called every frame of the game
-      
-        rb.MovePosition(transform.position + 
-            new Vector3(Input.GetAxis("Horizontal"),
-            Input.GetAxis("Vertical"),
-            0) * speed * Time.deltaTime);
+
+        if (control)
+        {
+
+            rb.MovePosition(transform.position + new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime);
+        }
 	}
 }
